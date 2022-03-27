8e0fa9e602f2eb178be5b683eb728190824dd5f2
Tewa-1337 <60029473+Tewa-1337@users.noreply.github.com>
2022-03-27 23:44:23 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using DigitalSkills2017.Helper;
+using DigitalSkills2017.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DigitalSkills2017
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new AuthPage());
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MainFrame.GoBack();
+            }
+            catch
+            {
+                MessageBox.Show("Это первая страница!");
+            }
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+        }
+
+        private void MainFrame_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            
+        }
+    }
+}
